3eb5f0fe238d23e3c743bdd23fc6b248bb3e0636
«Natalia <kolodina_melbyk@mail.ru>
2023-02-14 19:12:19 +0400

    add task2 CountOfDigits

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89456 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"{num} -> {NumOfDigits(num)}");
+
+int NumOfDigits(int number)
+{
+    int result = 1;
+    while (number > 10)
+    {
+        number = number / 10;
+        result++;
+    }
+    return result;
+}

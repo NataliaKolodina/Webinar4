2a0cfabb971d708bddacd1178c57b7837b13a551
«Natalia <kolodina_melbyk@mail.ru>
2023-02-14 19:22:25 +0400

    add task3 MultiplicationOfNums

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"{N} -> {MultiplicationOfNums(N)}");
+
+int MultiplicationOfNums(int number)
+{
+    int result = 1;
+    while (number > 1)
+    {
+        result *= number;
+        number--;
+    }
+    return result;
+}
